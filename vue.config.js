7ce78ab4b6f94adedfff6705153cebd560720212
Cuke7 <cuke.dev@mail.com>
2021-04-24 15:35:54 +0200

    icons

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,29 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
-}
+    transpileDependencies: ["vuetify"],
+    pwa: {
+        manifestOptions: {
+            name: "Auditere",
+            short_name: "Auditere",
+            start_url: "./",
+            display: "standalone",
+            theme_color: "#000080",
+            orientation: "portrait",
+            background_color: "#000080",
+            icons: [
+                {
+                    src: "images/app_icon.png",
+                    sizes: "846x846",
+                    type: "image/png",
+                    purpose: "any maskable",
+                },
+            ],
+        },
+        iconPaths: {
+            favicon32: null,
+            favicon16: null,
+            appleTouchIcon: null,
+            maskIcon: null,
+            msTileImage: null,
+        },
+    },
+};
