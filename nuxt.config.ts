3e565edcaa62f3ed524629ce557de3ef18758e1a
Cuke7 <cuke.dev@mail.com>
2022-11-08 17:01:13 +0100

    V1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,33 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss'],
+    modules: ['@nuxtjs/tailwindcss', '@kevinmarrec/nuxt-pwa'],
+    pwa: {
+        workbox: {
+            enabled: true
+        },
+        manifest: {
+            name: 'Auditere',
+            short_name: 'Auditere',
+            background_color: "#000000",
+            theme_color: "#000000",
+            icons: [
+                {
+                    src: 'bird.png',
+                    sizes: '1200x1200',
+                    type: 'image/png',
+                    purpose: "maskable"
+                },
+                {
+                    src: 'bird.png',
+                    sizes: '1200x1200',
+                    type: 'image/png',
+                    purpose: "any"
+                }
+            ]
+        }
+
+
+    },
     ssr: true,
     app: {
         pageTransition: { name: 'page', mode: 'out-in' }
