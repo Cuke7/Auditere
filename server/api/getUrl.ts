859b582f81d8c7f8afe01fe3bcb07c07e518c832
Cuke7 <cuke.dev@mail.com>
2022-11-10 17:37:42 +0100

    test stream

--- server/api/getUrl.ts
+++ server/api/getUrl.ts
@@ -23,8 +23,9 @@ export default defineEventHandler(async (event) => {
         format: format,
     })
     // event.respondWith(stream)
-    appendResponseHeader(event, 'Content-Type', "audio/mpeg")
-    appendResponseHeader(event, "Content-Length", size)
+    setResponseHeader(event, 'Content-Type', "audio/mpeg")
+    // stream.pipe(event.res)
+    // appendResponseHeader(event, "Content-Length", size)
 
 
     // setResponseHeader(event, 'Content-Type', "audio/mpeg")
