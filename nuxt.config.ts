4aa734a89ce78c0cde14776f1f474ec1797f8795
Cuke7 <cuke.dev@mail.com>
2022-11-10 19:33:47 +0100

    test scroll

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
     },
     pwa: {
         workbox: {
-            enabled: false
+            enabled: true
         },
         manifest: {
             name: 'Auditere',
